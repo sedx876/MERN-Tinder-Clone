8a305370ef50eef87fad3f98bc97eb56a0c6c74c
sedx876 <swatkins876@gmail.com>
2020-11-04 10:51:06 -0500

    Code cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import Cors from 'cors'
 
 //app config
 const app = express()
@@ -7,8 +9,8 @@ const port = process.env.PORT || 8001
 const connection_url = `mongodb+srv://admin:41vlxBYFyNEMkrvq@cluster0.sx02s.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 //middleware
-
-
+app.use(express.json())
+app.use(Cors())
 //db config
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
@@ -18,6 +20,28 @@ mongoose.connect(connection_url, {
 
 //api endpoints
 app.get('/', (req, res) => res.status(200).send('Hello'))
+
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err){
+      res.status(500).send(err)
+    }else{
+      res.status(201).send(data)
+    }
+  })
+})
+
+app.get('/tinder/cards', (req, res) => {
+  Cards.find((err, data) => {
+    if (err){
+      res.status(500).send(err)
+    }else{
+      res.status(200).send(data)
+    }
+  })
+})
 
 //listener
 app.listen(port, () => console.log('listening'))
--- frontend/src/components/TinderCards.js
+++ frontend/src/components/TinderCards.js
@@ -1,36 +1,19 @@
-import { SwipeableDrawer } from '@material-ui/core'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TinderCard from 'react-tinder-card'
 import '../styles/TinderCards.css'
+import axios from './axios'
 
 function TinderCards() {
 
-  const [people, setPeople] = useState([
-    {
-      name: 'Tom Hiddleston',
-      url: 'https://tvguide1.cbsistatic.com/mediabin/showcards/celebs/s-u/thumbs/tom-hiddleston-325607_828x1104.png',
-    },
-    {
-      name: 'RuPaul',
-      url: 'https://media2.s-nbcnews.com/j/newscms/2020_15/3304406/200410-rupaul-scret-drag-race-se-436p_c2e2f6ae6350d5d49a4f97cf184bfb21.fit-760w.jpg',
-    },
-    {
-      name: 'Arya Stark',
-      url: 'https://pyxis.nymag.com/v1/imgs/846/9bb/440e83edacba3579e42bb6ad20860b50b0-18-arya-stark.rsquare.w700.jpg',
-    },
-    {
-      name: 'Sandor Clegane',
-      url: 'https://www.redwolf.in/image/catalog/artwork-Images/badges/hound-badge-artwork.jpg',
-    },
-    {
-      name: 'Tormund Giantsbane',
-      url: 'https://laughingsquid.com/wp-content/uploads/2017/08/the-career-of-kristofer-hivju-the-actor-who-plays-tormund-giantsbane-on-game-of-thrones.jpg',
-    },
-    {
-      name: 'Brienne of Tarth',
-      url: 'https://www.flare.com/wp-content/uploads/2019/04/got-inline.jpg',
-    },
-  ])
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/tinder/cards')
+      setPeople(req.data)
+    }
+    fetchData()
+  }, [])
 
   const swiped = (direction, nameToDelete) => {
     console.log('removing: ' + nameToDelete)
@@ -52,7 +35,7 @@ function TinderCards() {
             onSwipe={(dir) => swiped(dir, person.name)}
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <div style={{ backgroundImage: `url(${person.url})`}}
+            <div style={{ backgroundImage: `url(${person.imgUrl})`}}
               className="card">
               <h3>{person.name}</h3>
             </div>
